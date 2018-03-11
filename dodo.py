1f01390cf7fcdd206972677c4afb037f01c6c798
C. E. Ball <ceball@gmail.com>
2018-03-11 23:16:10 +0000

    debugging

--- dodo.py
+++ dodo.py
@@ -1,3 +1,29 @@
 from pyct import *
 
-DOIT_CONFIG['backend'] = 'sqlite3'
+# debugging
+def tmp(task,values):
+    print(os.path.join(sys.prefix,"envs",task.options['name']))
+    print(os.path.exists(os.path.join(sys.prefix,"envs",task.options['name'])))
+    return os.path.exists(os.path.join(sys.prefix,"envs",task.options['name']))
+
+def task_create_env():
+    python = {
+        'name':'python',
+        'long':'python',
+        'type':str,
+        'default':'3.6'}
+
+    name = {
+        'name':'name',
+        'long':'name',
+        'type':str,
+        'default':'test-environment'}
+
+    return {
+        'params': [python,name],
+        # TODO: this assumes env created in default location...but
+        # apparently any conda has access to any other conda's
+        # environments (?!) plus those in ~/.conda/envs (?!)
+        'uptodate': [tmp],        
+#        'uptodate': [lambda task,values: os.path.exists(os.path.join(sys.prefix,"envs",task.options['name']))],
+        'actions': ["conda create -y --name %(name)s python=%(python)s"]}
